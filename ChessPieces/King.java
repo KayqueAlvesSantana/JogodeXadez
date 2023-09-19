ce80e1164ff3dcf1381bb18eb0797714b1cdb7ae
Kayque Alves Santana <121639116+KayqueAlvesSantana@users.noreply.github.com>
2023-09-19 08:48:02 -0300

    Add files via upload

---
+++ ChessPieces/King.java
@@ -0,0 +1,17 @@
+package ChessPieces;
+
+import TabuleirodaPartida.Board;
+import Chess.ChessPiece;
+import Chess.Color;
+
+
+public class King extends ChessPiece {
+    public King(Board board, Color color){
+        super(board, color);
+    }
+
+    @Override
+    public String toString(){
+        return "K";
+    }
+}
---
+++ testes.java
@@ -0,0 +1,9 @@
+import org.example.Main;
+import org.junit.Test;
+
+public class testes {
+    @Test
+    public void somazinha(){
+        Main numeros = new Main(2,2);
+    }
+}
